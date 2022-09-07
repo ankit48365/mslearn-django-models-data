97bda068d8fddcf2031f43538751f7e93847b586
ankit K <ankit48365@gmail.com>
2022-09-07 13:57:40 -0400

    A

--- starter/dog_shelters/models.py
+++ starter/dog_shelters/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.urls import reverse
+
 
 # Create your models here
 class Shelter(models.Model):
@@ -14,3 +16,6 @@ class Dog(models.Model):
     intake_date = models.DateTimeField(auto_now_add=True)
     def __str__(self):
         return self.name
+    # TODO: Add get_absolute_url
+    def get_absolute_url(self):
+        return reverse('dog_detail', kwargs={"pk": self.pk})
